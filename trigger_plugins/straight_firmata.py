10b45180d152da81ee948cd82d06c5eabcf858ce
HMYamano <122147942+HMYamano@users.noreply.github.com>
2024-02-19 13:51:34 +0900

    change fimata program

--- trigger_plugins/straight_firmata.py
+++ trigger_plugins/straight_firmata.py
@@ -1,6 +1,6 @@
 import time
 
-import asovi.arduino as ard
+import yoru.libs.arduino as ard
 import serial
 import serial.tools.list_ports
 
@@ -8,10 +8,10 @@ import serial.tools.list_ports
 class trigger_condition:
     def __init__(self):
         print("trigger_command")
-        myArduino = ard.dio(comport="COM3", doCh_IDs=[13])
+        self.myArduino = ard.dio(comport="COM3", doCh_IDs=[13])
 
     def trigger(self, tri_cl, in_cl, ser, results, now):
         if tri_cl in in_cl:
-            myArduino.writeDO_all(1)
+            self.myArduino.writeDO_all(1)
         else:
-            myArduino.writeDO_all(0)
+            self.myArduino.writeDO_all(0)
