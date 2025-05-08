c2b8b66c712f5407ede90dd86e0752c2ee1b05c6
Hayato M Yamanouchi <122147942+HMYamano@users.noreply.github.com>
2025-05-08 19:34:52 +0900

    add virtual serial transport plugin

--- trigger_plugins/straight.py
+++ trigger_plugins/straight.py
@@ -5,11 +5,17 @@ import serial.tools.list_ports
 
 
 class trigger_condition:
-    def __init__(self):
+    def __init__(self, m_dict, rate=9600):
         print("trigger_command")
 
-    def trigger(self, tri_cl, in_cl, ser, results, now):
+        self.quit = False
+        comport = m_dict.get("arduino_com")
+        self.ser = serial.Serial(comport, rate,
+                                 timeout=0.1,
+                                 parity=serial.PARITY_NONE)
+
+    def trigger(self, tri_cl, in_cl, arduino, results, now):
         if tri_cl in in_cl:
-            ser.write(b"1")
+            self.ser.write(b"1")
         else:
-            ser.write(b"0")
+            self.ser.write(b"0")
