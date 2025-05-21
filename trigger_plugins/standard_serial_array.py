ca7783c5401264f0be095019447c6d406349c423
Hayato M Yamanouchi <122147942+HMYamano@users.noreply.github.com>
2025-05-21 10:39:01 +0900

    add serial plugin

--- trigger_plugins/standard_serial_array.py
+++ trigger_plugins/standard_serial_array.py
@@ -4,11 +4,11 @@ import serial
 import serial.tools.list_ports
 import numpy as np
 
-COM_PORT = "COM3"
+COM_PORT = "COM21"
 
 class trigger_condition:
 
-    def __init__(self, m_dict, comport = COM_PORT, rate=9600):
+    def __init__(self, m_dict, comport = COM_PORT, rate=115200):
         self.m_dict = m_dict
         
         self.quit = False
@@ -19,8 +19,14 @@ class trigger_condition:
         # self.ser.write(b'# INITIALIZED')
         print("trigger_command")
 
+    def send_array(self, data=[]):
+        data = ','.join(map(str, data))
+        self.ser.write(f'{data}\r'.encode())
+        self.ser.flush()
+        return 
+
     def trigger(self, tri_cl, in_cl, arduino, results, now):
         if tri_cl in in_cl:
-            self.ser.write(b"1")
+            self.send_array(np.array([1]))
         else:
-            self.ser.write(b"0")
+            self.send_array(np.array([0]))
